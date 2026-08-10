19aba660038efec5875f647aad867aaedcee521c
Samuel Gabriel <samuelgabriel199@gmail.com>
2026-08-10 10:17:29 +0100

    Improve search phrase normalization

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,24 +1,19 @@
 import string
 
-
 SYNONYMS = {
     "maximum age": "age",
     "age limit": "age",
     "age requirement": "age",
     "how old": "age",
     "old can i be": "age",
+
     "serve nysc": "nysc service",
     "serving nysc": "nysc service",
 
-    "what should i take to nysc camp": "camp documents",
-    "what should i take to camp": "camp documents",
-    "what should i bring to nysc camp": "camp documents",
-    "what should i bring to camp": "camp documents",
-
-    "what should i take": "camp documents",
-    "what should i bring": "camp documents",
-    "things to take": "camp documents",
-    "things to bring": "camp documents",
+    "what should i take": "documents",
+    "what should i bring": "documents",
+    "things to take": "documents",
+    "things to bring": "documents",
     "bring to camp": "camp documents",
     "take to camp": "camp documents",
 
@@ -27,8 +22,21 @@ SYNONYMS = {
     "required documents": "documents",
     "documents do i need": "documents",
     "what do i need": "documents",
-}
 
+    "what should i take to nysc camp": "camp documents",
+    "what should i take to camp": "camp documents",
+    "what should i bring to nysc camp": "camp documents",
+    "what should i bring to camp": "camp documents",
+
+    "doesnt have to": "not required to",
+    "dont have to": "not required to",
+    "does not have to": "not required to",
+    "do not have to": "not required to",
+    "not required to": "not required to",
+
+    "what does nysc mean": "what does nysc stand for",
+    "what is nysc mean": "what does nysc stand for",
+}
 
 def preprocess_text(text):
     """
@@ -41,10 +49,7 @@ def preprocess_text(text):
         str.maketrans("", "", string.punctuation)
     )
 
-    for phrase in sorted(SYNONYMS, key=len, reverse=True):
-        text = text.replace(
-            phrase,
-            SYNONYMS[phrase]
-        )
+    for phrase, replacement in SYNONYMS.items():
+        text = text.replace(phrase, replacement)
 
     return text
